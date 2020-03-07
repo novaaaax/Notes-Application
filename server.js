4b1cc1bb9eda0a0a6569a6bc43cd16135b3aebb6
Edwin <edwingenova@gmail.com>
2020-03-06 19:43:35 -0500

    cleaned up code a bit

--- server.js
+++ server.js
@@ -42,10 +42,7 @@ app.delete("/api/notes/:id", function (req, res) {
         }
     })
     return res.json(false);
-
 });
-
-
 
 app.listen(PORT, function () {
     console.log("App listening on PORT" + PORT);
